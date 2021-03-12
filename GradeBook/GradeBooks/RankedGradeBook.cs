b944c6864456cfb358f569e424392e8921549d3f
Greg A <79934518+greg4502@users.noreply.github.com>
2021-03-12 16:16:30 -0700

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
